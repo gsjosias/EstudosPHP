f23d7543cd48b9a771138436bab305645ccf9fed
gsjosias <josiiasgoncalves97@gmail.com>
2022-07-25 02:49:33 -0300

    Estudo básico sobre a impressão de variáveis através do echo .

---
+++ Aula06-EchoePrint/index.php
@@ -0,0 +1,8 @@
+<?php 
+$nome = "Dimitri";
+$sobrenome = "Teixeira";
+
+echo "O nome do usuário é $nome $sobrenome"; //As variáveis só são reconhecidas no retorno do echo quando são utilizadas aspas duplas
+
+echo 'O nome do usuário é' .' '.$nome.' '.$sobrenome; //Imprimindo variáveis através das aspas simples
+?>
