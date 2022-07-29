29634218f2d8f23b4f3c8644409d595ebae6cd15
gsjosias <josiiasgoncalves97@gmail.com>
2022-07-29 02:28:48 -0300

    Aula sobre operadores

---
+++ Aula12-Operadores/index.php
@@ -0,0 +1,22 @@
+/* OPERADORES 
+ex: + - * / = ++ -- += &&
+1)Operadores Aritiméticos(matemáticos)
++ - * /
+2)Operadores de atribuição
+=
+3)Operadores de sequência (contatenação)
+    $nome = "Dimitri";
+    $sobrenome = "Gonçalves";
+    $nome_completo = $nome.$sobrenome;
+    echo $nome_completo
+4)Operadores de comparação
++= -= == === != !==
+5)Operador condicional (Ternário)
+condição ? condigoUm : codigoDois;
+6)Operadores lógicos
+and or xor(se a ou b são verdadeiros, mas não ambos) ! && ||
+
+
+
+
+*/
