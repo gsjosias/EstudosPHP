520d6b195b9312c4e4e3e272f967eadadb464ef0
gsjosias <josiiasgoncalves97@gmail.com>
2022-07-30 10:26:21 -0300

    Aula sobre laços de repetição.

---
+++ Aula14-LoopsWhileDoWhileForeForeach/index.php
@@ -0,0 +1,36 @@
+<?php 
+   /* LAÇOS DE REPETIÇÃO */
+/* 
+   //WHILE
+   $x = 0;
+   while($x <= 100){
+    echo "O número é : $x <br>";
+    $x+=10;
+   } 
+   Executa uma função a partir de uma determinada condição
+
+    //DO...WHILE
+
+    $x = 1;
+    do {
+        echo "O número é: $x <br>";
+        $x++;
+    }while($x <= 5);
+
+    Executa uma ação e só torna a executá-la a partir de uma determinada condição
+    
+    //FOR
+
+    for($x = 0; $x <= 10; $x++){
+        echo "O número é: $x<br>";
+    }
+    Executa uma ação numa quantidade de vezes pré determinada
+    
+    //FOREACH 
+    $cores = ["azul", "amarelo", "verde", "vermelho"];
+    foreach($cores as $valor){
+        echo "A cor é $valor<br>";
+    }
+    //Percorre a matriz, classificando cada item com o nome pré-determinado e exibindo um por vez
+    */
+?>
